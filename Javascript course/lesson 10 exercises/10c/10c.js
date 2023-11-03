187a253480b878d3619f4a9f56a7ac5a0dd167a1
Rita Ogada <ritaogada@gmail.com>
2023-11-03 17:01:57 +0300

    lesson 10 exercises
    A few exercises on HTML, CSS and JavaScript

---
+++ Javascript course/lesson 10 exercises/10c/10c.js
@@ -0,0 +1,10 @@
+const findClass = document.querySelector('.js-button');
+
+
+console.log(findClass.classList.contains('js-button'));
+
+ const button = document.querySelector('.js-button');
+console.log(button.classList.contains('js-button'));
+
+const button2 = document.querySelector('.js2-button');
+console.log(button2.matches('.js-button'));
