3943ce87b7b274af09791ba685474bda5786abd4
Rita Ogada <ritaogada@gmail.com>
2023-12-02 17:02:37 +0300

    Update 11-lesson-exercise.js

--- Javascript course/arrays and loops/lesson 11 exercises/11-lesson-exercise.js
+++ Javascript course/arrays and loops/lesson 11 exercises/11-lesson-exercise.js
@@ -1,3 +1,5 @@
+/*
+
 // lesson 11a: Modifying the last value in the array to 99
 
 const nums = [10, 20, 30];
@@ -61,3 +63,67 @@ console.log(arraySwap(['hello', 'world', 'how', 'are', 'you']))
     console.log(num4);
     num4--;
   }
+
+  // lesson 11g: creating a loop that takes an array of numbers and creates a new array where each number is increased by 1
+
+  const originalArray = [1, 2, 3];
+
+  const newArray = [];
+
+  for (let x = 0; x < originalArray.length; x++) {
+    newArray.push(originalArray[x] + 1);
+  }
+
+  console.log(newArray);
+
+  // lesson 11g using the 'map' function
+
+ const Array1 = [1, 2, 3];
+
+  const Array2 = Array1.map(number => number + 1);
+
+  console.log(Array2); 
+  
+  */
+
+  // lesson 11h: create a function  addOne(array) that takes an array of numbers and returns an array where each number is increased by 1
+
+  
+
+  function addOne(array) {
+    
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+      newArray.push(array[i] + 1);
+    }  
+
+    return newArray;
+
+  }
+
+
+    console.log(addOne[1, 2, 3]);
+    console.log(addOne[-2, -1, 0, 99]);
+
+ /*
+
+ function increaseNumbersByOne(originalArray) {
+  // New array to store the incremented values
+  const newArray = [];
+
+  // Loop through each element in the original array
+  for (let i = 0; i < originalArray.length; i++) {
+    // Increment each element by 1 and push it to the new array
+    newArray.push(originalArray[i] + 1);
+  }
+
+  // Return the new array
+  return newArray;
+}
+
+// Example usage:
+const originalArray = [1, 2, 3];
+const newArray = increaseNumbersByOne(originalArray);
+console.log(newArray); // Output: [2, 3, 4]
+*/
